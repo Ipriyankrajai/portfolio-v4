e4a07beb85a3fafd653e8616329a6fdca5fc3656
Ipriyankrajai <priyank73@hotmail.com>
2025-01-11 20:13:06 +0530

    chore: site config

---
+++ config/index.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: "Priyank Rajai | Full-Stack Developer | Open Source Steward",
+  description:
+    "I'm a passionate Full Stack Developer from India with expertise in building web applications using React.js, Next.js, Node.js, PostgreSQL, MongoDB, Langchain, OpenAI, LLMs, and Web3.js.",
+  url:
+    process.env.NODE_ENV === "production"
+      ? "https://priyankrajai.com"
+      : "http://localhost:3000",
+};

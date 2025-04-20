dd043501915346b634e0f6035f8ccfcac3c074d6
Ipriyankrajai <priyank73@hotmail.com>
2025-04-20 23:33:15 +0530

    feat: footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-8">
+      <p className="text-center text-sm font-semibold text-neutral-400">
+        Crafted by Priyank Rajai &copy; {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
+}

c6355b66dd1ca01f098fbd2eb44067935536e4af
Ipriyankrajai <priyank73@hotmail.com>
2025-05-16 15:13:52 +0530

    feat: add status badge to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,16 @@ export default function Footer() {
       <p className="text-center text-sm font-semibold text-neutral-400">
         Crafted by Priyank Rajai &copy; {new Date().getFullYear()}
       </p>
+      <div className="flex justify-center mt-2">
+        <iframe
+          src="https://status.priyankrajai.com/badge?theme=dark"
+          width="185"
+          height="30"
+          frameBorder="0"
+          scrolling="no"
+          style={{ colorScheme: "normal" }}
+        ></iframe>
+      </div>
     </footer>
   );
 }
